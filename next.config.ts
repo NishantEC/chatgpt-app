b3aec6dee668356c2b277a34580cb59369a67765
Nishant Gupta <nishant.gupta@healthifyme.com>
2025-10-15 00:27:39 +0530

    refactor

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import { baseURL } from "./baseUrl";
 
 const nextConfig: NextConfig = {
   assetPrefix: baseURL,
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/oauth-protected-resource",
+        destination: "/api/.well-known/oauth-protected-resource",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
