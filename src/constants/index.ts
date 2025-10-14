58ca4ddfc46630ab5a8172b5edf5dc2b6112c8aa
Nishant Gupta <nishant.gupta@healthifyme.com>
2025-10-14 19:34:31 +0530

    Revert "for vercel"
    This reverts commit b40744beb53c74df850856cfbcc4dcce92acfb36.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,1 @@
-export const HOST_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : process.env.HOST_URL || "https://ed4df19a03a9.ngrok-free.app";
+export const HOST_URL = "https://ed4df19a03a9.ngrok-free.app";
